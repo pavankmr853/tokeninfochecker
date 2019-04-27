33f22689a15f553257eaa2a8e7fd1f96ce46ff9c
pavankmr853 <pavankmr853@gmail.com>
2019-04-27 19:09:23 +0530

    first commit

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
-import  json
 from web3 import Web3
 
-infura_url = "https://ropsten.infura.io/v3/6cc62c163d4c4dc4bc3aa7ca866b8866"
-web3 = Web3(Web3.HTTPProvider(infura_url))
+ganache_url = "HTTP://127.0.0.1:7545"
+web3 = Web3(Web3.HTTPProvider(ganache_url))
+
+account_1 = "0x09566dEFBD91D9C218c40bD89Dd04eC5C20b6cab"
+account_2 = "0x19081D4665879E49F408A1d386Df9215F3C337d2"
+
+private_key = "af63980c120dcc88b316c39b5b572916a388eb844cf63834b14283e5b28c64cd"
 
 
-abi = json.loads('give the abi of your contract')
-address = "give the address of your contract"
+nonce = web3.eth.getTransactionCount(account_1)
 
-contract = web3.eth.contract(address=address, abi=abi)
-print(contract)
-totalSupply = contract.functions.totalSupply().call()
-print(web3.fromWei(totalSupply, 'ether'))
-print(contract.functions.name().call())
-print(contract.functions.symbol().call())
-balance = contract.functions.balanceOf('give the address of the account whom you want check the balance').call()
-print(web3.fromWei(balance, 'ether'))
+tx = {
+'nonce': nonce,
+'to': account_2,
+'value': web3.toWei(1, 'ether'),
+'gas': 2000000,
+'gasPrice': web3.toWei('50', 'gwei')
+}
+
+signed_tx = web3.eth.account.signTransaction(tx, private_key)
+
+tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+
+print(web3.toHex(tx_hash))
